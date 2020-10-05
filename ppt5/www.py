fef590d265756187230d70305d5155ad5c395f10
Jeffrey Liu <17771770679@163.com>
2020-10-05 20:05:35 +0800

    5.1账号管理相关页面OK

--- ppt5/www.py
+++ ppt5/www.py
@@ -1,7 +1,9 @@
 from application import app
 from web.controllers.index import route_index
 from web.controllers.user.User import route_user
+from web.controllers.account.Account import route_account
 
 # 每次增加新的模块都要注册
 app.register_blueprint(route_index,url_prefix='/')
 app.register_blueprint(route_user,url_prefix='/user')
+app.register_blueprint(route_account,url_prefix='/account')
