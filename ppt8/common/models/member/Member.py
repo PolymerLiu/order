0f9b1dd4e101dfcedffada921648c525956b4b9a
Jeffrey Liu <17771770679@163.com>
2020-10-13 19:13:13 +0800

    8.3注册并将用户信息存储到数据库

---
+++ ppt8/common/models/member/Member.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+from application import db
+
+
+class Member(db.Model):
+    __tablename__ = 'member'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(100), nullable=False, server_default=db.FetchedValue(), info='???')
+    mobile = db.Column(db.String(11), nullable=False, server_default=db.FetchedValue(), info='??????')
+    sex = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='?? 1?? 2??')
+    avatar = db.Column(db.String(200), nullable=False, server_default=db.FetchedValue(), info='????')
+    salt = db.Column(db.String(32), nullable=False, server_default=db.FetchedValue(), info='??salt')
+    reg_ip = db.Column(db.String(100), nullable=False, server_default=db.FetchedValue(), info='??ip')
+    status = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='?? 1??? 0???')
+    updated_time = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue(), info='????????')
+    created_time = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue(), info='????')
--- ppt8/web/controllers/api/Member.py
+++ ppt8/web/controllers/api/Member.py
@@ -1,7 +1,10 @@
-from web.controllers.api import route_api
 from flask import Blueprint,request,jsonify
-from application import app
+from application import app,db
 import requests,json
+from web.controllers.api import route_api
+from common.models.member.Member import Member
+from common.models.member.OauthMemberBind import OauthMemberBind
+from common.libs.Helper import getCurrentDate
 
 @route_api.route('/member/login',methods=['GET','POST'])
 def login():
@@ -17,6 +20,36 @@ def login():
   r = requests.get(url)
   res = json.loads(r.text)
   openid = res['openid']
-  app.logger.info(res)
 
+  nickname = req['nickName'] if 'nickName' in req else ''
+  sex = req['gender'] if 'gender' in req else ''
+  avatar = req['avatarUrl'] if 'avatarUrl' in req else ''
+
+  # 判断是否已经绑定过，如果已经绑定则直接返回一些信息
+  bind_info = OauthMemberBind.query.filter_by(openid=openid,type=1).first()
+  if not bind_info:
+    model_member = Member()
+    model_member.nickname = nickname
+    model_member.sex = sex
+    model_member.avatar = avatar
+    model_member.reg_ip = ''
+    model_member.salt = ''
+    model_member.updated_time = model_member.updated_time = getCurrentDate()
+    db.session.add(model_member)
+    db.session.commit()
+
+    model_bind = OauthMemberBind()
+    model_bind.member_id = model_member.id
+    model_bind.type = 1
+    model_bind.client_type = 'weixin'
+    model_bind.openid = openid
+    model_bind.updated_time = model_bind.updated_time = getCurrentDate()
+    model_bind.extra = ''
+    db.session.add(model_bind)
+    db.session.commit()
+
+    bind_info = model_bind
+
+  member_info = Member.query.filter_by(id=bind_info.member_id).first()
+  resp['data'] = {'nickname':member_info.nickname}
   return jsonify(resp)
