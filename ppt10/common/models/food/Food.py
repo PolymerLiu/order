56e4b4d93ae8ed841cc78e4b8d798e8e517687f5
Jeffrey Liu <17771770679@163.com>
2020-10-17 12:21:39 +0800

    10.1 共享tab，创建表及初始化model

---
+++ ppt10/common/models/food/Food.py
@@ -0,0 +1,20 @@
+from application import db
+
+class Food(db.Model):
+    __tablename__ = 'food'
+
+    id = db.Column(db.Integer, primary_key=True)
+    cat_id = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='??id')
+    name = db.Column(db.String(100), nullable=False, server_default=db.FetchedValue(), info='????')
+    price = db.Column(db.Numeric(10, 2), nullable=False, server_default=db.FetchedValue(), info='????')
+    main_image = db.Column(db.String(100), nullable=False, server_default=db.FetchedValue(), info='??')
+    summary = db.Column(db.String(10000), nullable=False, server_default=db.FetchedValue(), info='??')
+    stock = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='???')
+    tags = db.Column(db.String(200), nullable=False, server_default=db.FetchedValue(), info='tag?????","??')
+    status = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='?? 1??? 0???')
+    month_count = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='?????')
+    total_count = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='????')
+    view_count = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='?????')
+    comment_count = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='????')
+    updated_time = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue(), info='??????')
+    created_time = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue(), info='??????')
---
+++ ppt10/common/models/food/FoodCat.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+from application import db
+
+class FoodCat(db.Model):
+    __tablename__ = 'food_cat'
+
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(50), nullable=False, server_default=db.FetchedValue(), info='????')
+    weight = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='??')
+    status = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue(), info='?? 1??? 0???')
+    updated_time = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue(), info='????????')
+    created_time = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue(), info='????')
